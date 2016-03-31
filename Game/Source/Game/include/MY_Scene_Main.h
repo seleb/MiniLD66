04a43dcbb28647a3f087f38096ffb7ea1581a1df
seleb <sean.s.leblanc@gmail.com>
2016-03-31 12:49:33 -0400

    cell selection, debugging stuff, variable size board

--- Game/Source/Game/include/MY_Scene_Main.h
+++ Game/Source/Game/include/MY_Scene_Main.h
@@ -2,15 +2,27 @@
 
 #include <MY_Scene_Base.h>
 
+class BulletWorld;
+class BulletDebugDrawer;
 class DirectionalLight;
 
 class MY_Scene_Main : public MY_Scene_Base{
 public:
+	// The scene's physics world
+	BulletWorld * bulletWorld;
+	// used to draw wireframes showing physics colliders, transforms, etc
+	BulletDebugDrawer * bulletDebugDrawer;
+
+
 	ComponentShaderBase * diffuseShader;
 
 	std::vector<Shader *> shaders;
 	
 	DirectionalLight * sun;
+	NodeBulletBody * selectedCell;
+	std::map<NodeBulletBody *, glm::vec3> cellGrid;
+
+	MeshEntity * cellHighlight;
 
 	OrthographicCamera * gameCam;
 
@@ -18,4 +30,9 @@ public:
 
 	MY_Scene_Main(Game * _game);
 	~MY_Scene_Main();
+	
+	// overriden to add physics debug drawing
+	virtual void enableDebug() override;
+	// overriden to remove physics debug drawing
+	virtual void disableDebug() override;
 };
--- Game/Source/Game/src/MY_Scene_Main.cpp
+++ Game/Source/Game/src/MY_Scene_Main.cpp
@@ -9,12 +9,26 @@
 #include <MeshFactory.h>
 
 #include <DirectionalLight.h>
+#include <PointLight.h>
 #include <NumberUtils.h>
+#include <BulletMeshEntity.h>
+#include <BulletDebugDrawer.h>
+#include <NodeBulletBody.h>
+
+#define SIZE 32
 
 MY_Scene_Main::MY_Scene_Main(Game * _game) :
 	MY_Scene_Base(_game),
-	diffuseShader(new ComponentShaderBase(true))
+	diffuseShader(new ComponentShaderBase(true)),
+	bulletWorld(new BulletWorld()),
+	bulletDebugDrawer(new BulletDebugDrawer(bulletWorld->world)),
+	selectedCell(nullptr)
 {
+	// Setup the debug drawer and add it to the scene
+	bulletWorld->world->setDebugDrawer(bulletDebugDrawer);
+	childTransform->addChild(bulletDebugDrawer, false);
+	bulletDebugDrawer->setDebugMode(btIDebugDraw::DBG_NoDebug);
+
 	diffuseShader->addComponent(new ShaderComponentMVP(diffuseShader));
 	diffuseShader->addComponent(new ShaderComponentDiffuse(diffuseShader, false));
 	diffuseShader->addComponent(new ShaderComponentTexture(diffuseShader));
@@ -27,29 +41,52 @@ MY_Scene_Main::MY_Scene_Main(Game * _game) :
 	childTransform->addChild(sun);
 	lights.push_back(sun);
 
+	PointLight * pl = new PointLight(glm::vec3(0.1f), 0, 0.005f, -1);
+	sun->firstParent()->addChild(pl, false);
+	lights.push_back(pl);
+
 	// add a cube to the light too so that we can see it without having to use the debug mode
 	sun->childTransform->addChild(new MeshEntity(MeshFactory::getCubeMesh(), baseShader),false);
 
 
 	// TERRAIN
 	QuadMesh * cubeMesh = MeshFactory::getCubeMesh();
-	Transform * terrain = new Transform();
-	terrain->translate(0, 0.5f, 0, false);
-	cubeMesh->applyTransformation(terrain);
-		
-	childTransform->addChild(terrain, false);
-	terrain->translate(-8, 0, -8, false);
-	for(unsigned long int x = 0; x < 16; ++x){
-	for(unsigned long int y = 0; y < 16; ++y){
-		MeshEntity * me = new MeshEntity(cubeMesh, diffuseShader);
-		terrain->addChild(me)->translate(x, 0, y)->scale(glm::vec3(1, sweet::NumberUtils::randomInt(1, 3)*0.33f, 1));
-	}
+	Transform t;
+	t.translate(0, 0.5f, 0, false);
+	cubeMesh->applyTransformation(&t);
+	MeshEntity * terrain = new MeshEntity(new QuadMesh(true), diffuseShader);
+	childTransform->addChild(terrain);
+	for(signed long int x = 0; x < SIZE; ++x){
+		for(signed long int y = 0; y < SIZE; ++y){
+			glm::vec3 cellPos(x - SIZE/2, 1, y - SIZE/2);
+			//MeshEntity * me = new MeshEntity(cubeMesh, diffuseShader);
+			//terrain->addChild(me)->translate(x, 0, y)->scale(glm::vec3(1, sweet::NumberUtils::randomInt(1, 3)*0.33f, 1));
+
+			BulletMeshEntity * collider = new BulletMeshEntity(bulletWorld, MeshFactory::getCubeMesh(), diffuseShader);
+			childTransform->addChild(collider);
+			collider->childTransform->translate(glm::vec3(cellPos.x, 0.5f, cellPos.z));
+			collider->freezeTransformation();
+			collider->setColliderAsBoundingBox();
+			collider->createRigidBody(0);
+			if(sweet::NumberUtils::randomFloat() > 0.75f){
+				float s = sweet::NumberUtils::randomInt(1, 2)*0.33f;
+				for(auto & v : collider->mesh->vertices){
+					v.y *= s;
+				}
+				cellPos.y *= s;
+			}
+			cellGrid[collider] = cellPos;
+
+
+			terrain->mesh->insertVertices(*collider->mesh);
+			collider->firstParent()->setVisible(false);
+		}
 	}
 
 
 
 	// CAMERA
-	gameCam = new OrthographicCamera(-16, 16, -8, 8, -1000, 1000);
+	gameCam = new OrthographicCamera(-SIZE, SIZE, -SIZE/2, SIZE/2, -1000, 1000);
 	cameras.push_back(gameCam);
 	//gameCam->childTransform->addChild(new CameraController(c));
 	childTransform->addChild(gameCam);
@@ -57,24 +94,86 @@ MY_Scene_Main::MY_Scene_Main(Game * _game) :
 	gameCam->yaw = -45;
 	gameCam->pitch = -45;
 	gameCam->roll = 0;
+	
+
+	uiLayer->addMouseIndicator();
+
+	cellHighlight = new MeshEntity(MeshFactory::getCubeMesh(), baseShader);
+	cellHighlight->childTransform->scale(glm::vec3(1, 0.1f, 1));
+	childTransform->addChild(cellHighlight);
+
+
+	bulletWorld->update(&sweet::step);
 }
 
 MY_Scene_Main::~MY_Scene_Main(){	
+	deleteChildTransform();
+
 	diffuseShader->decrementAndDelete();
+
+	delete bulletWorld;
 }
 
 void MY_Scene_Main::update(Step * _step){
-	//glm::uvec2 sd = sweet::getWindowDimensions();
+	glm::uvec2 sd = sweet::getWindowDimensions();
 	//gameCam->resize(0, sd.x, 0, sd.y);
 
 	// scene update
+	//bulletWorld->update(_step);
+
+	glm::vec3 s = activeCamera->screenToWorld(glm::vec3((float)mouse->mouseX()/sd.x, (float)mouse->mouseY()/sd.y, activeCamera->nearClip), sd);
+
+
+
+	//glm::vec3 pos = activeCamera->childTransform->getWorldPos();
+	btVector3 start(s.x, s.y, s.z);
+	btVector3 dir(activeCamera->forwardVectorRotated.x, activeCamera->forwardVectorRotated.y, activeCamera->forwardVectorRotated.z);
+	btVector3 end = start + dir*(activeCamera->farClip - activeCamera->nearClip);
+	btCollisionWorld::ClosestRayResultCallback rayCallback(start, end);
+	bulletWorld->world->rayTest(start, end, rayCallback);
+
+	NodeBulletBody * me;
+
+
+	// if we're not looking at anything, return nullptr
+	if(rayCallback.hasHit()){
+		me = static_cast<NodeBulletBody *>(rayCallback.m_collisionObject->getUserPointer());
+	}else{
+		me = nullptr;
+	}
+
+	if(selectedCell != me){
+		if(selectedCell != nullptr){
+			//selectedCell->setVisible(true);
+			cellHighlight->setVisible(false);
+		}
+		selectedCell = me;
+		if(selectedCell != nullptr){
+			glm::vec3 cellPos = cellGrid[selectedCell];
+			cellHighlight->setVisible(true);
+			cellHighlight->firstParent()->translate(cellPos, false);
+
+			//selectedCell->setVisible(false);
+		}
+	}
+
 	MY_Scene_Base::update(_step);
 
 	// update light position to make it orbit around the scene
-	float r = 8;
+	float r = SIZE/2;
 	float t = _step->time*0.25f;
 	sun->firstParent()->translate(glm::vec3(sin(t) * r, 0, cos(t) * r) + glm::vec3(0, 3, 0), false);
 	sun->childTransform->lookAt(glm::vec3(0));
 
 	sun->setIntensities(glm::vec3(glm::min(1.f, glm::sin(t) + 1.f), (glm::sin(t+15)+1)*0.5f, (glm::sin(t*2 + 15)+1)*0.4f+0.1f));
+}
+
+void MY_Scene_Main::enableDebug(){
+	MY_Scene_Base::enableDebug();
+	bulletDebugDrawer->setDebugMode(btIDebugDraw::DBG_MAX_DEBUG_DRAW_MODE);
+	childTransform->addChildAtIndex(bulletDebugDrawer, -1, false); // make sure the debug drawer is the last thing drawn
+}
+void MY_Scene_Main::disableDebug(){
+	MY_Scene_Base::disableDebug();
+	bulletDebugDrawer->setDebugMode(btIDebugDraw::DBG_NoDebug);
 }
